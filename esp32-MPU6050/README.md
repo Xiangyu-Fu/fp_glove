d99f6fc497e0d7584d1b79045c79655c86773c40
stan <stan_fu@163.com>
2023-12-27 17:08:35 +0100

    udpate example code

---
+++ esp32-MPU6050/README.md
@@ -0,0 +1,8 @@
+# MPU6050
+## Wire connecting
+```
+VCC --> 3v3
+GND --> GND
+SCL --> 22
+SDA --> 21
+```
--- esp32-MPU6050/src/mpu6050_task.cpp
+++ esp32-MPU6050/src/mpu6050_task.cpp
@@ -1,7 +1,7 @@
 #include "mpu6050_task.h"
 #include "Adafruit_MPU6050.h"
 
-Adafruit_MPU6050 mpu;
+// Adafruit_MPU6050 mpu;
 
 MPU6050Task::MPU6050Task(const uint8_t task_core) 
     : Task<MPU6050Task>("MPU6050Task", 8192, task_core) {
@@ -35,19 +35,19 @@ void MPU6050Task::publish(sensors_event_t aEvent) {
 }
 
 void MPU6050Task::run() {
-    while (true) {
-        sensors_event_t accel;
-        mpu.getEvent(&accel);
+    // while (true) {
+    //     sensors_event_t accel;
+    //     mpu.getEvent(&accel);
 
-        // Log the data if logger is set
-        if (logger_) {
-            logger_->log(accel);
-        }
+    //     // Log the data if logger is set
+    //     if (logger_) {
+    //         logger_->log(accel);
+    //     }
 
-        // Publish the data
-        publish(accel);
+    //     // Publish the data
+    //     publish(accel);
 
-        // Delay to control the frequency of readings
-        vTaskDelay(100 / portTICK_PERIOD_MS);
-    }
+    //     // Delay to control the frequency of readings
+    //     vTaskDelay(100 / portTICK_PERIOD_MS);
+    // }
 }
